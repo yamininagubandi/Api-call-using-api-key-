9b767701fcc1eb54ece87a0fb5eac014be74e17a
yamininagubandi <77452953+yamininagubandi@users.noreply.github.com>
2021-03-04 17:07:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 var request = new XMLHttpRequest();
-request.open('GET','https://restcountries.eu/rest/v2/all',true);
+
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+
 request.send();
-request.onload = function() {
-    var data = JSON.parse(this.response);
-    console.log(data);
+request.onload = function () {
+	var cd = JSON.parse(this.response);
 
-    for(var i in data)
-    {
-    
-    }
+	for (var i in cd) {
+		try {
+			var name = cd[i].name;
+			var lang = cd[i].latlng;
+			if (lang.length === 0) throw new Error('longitude not found');
+			wd(name, ...lang);
+		} catch (e) {
+			console.log('Invalid co-ordinate data for country: ' + name + ' ' + e.message);
+		}
+	}
+};
+var wd = function (name, lat, lang) {
+	var request = new XMLHttpRequest();
+	var url =
+		'https://api.openweathermap.org/data/2.5/weather?lat=' +
+		lat +
+		'&lon=' +
+		lang +
+		'&appid=a87b3645166900960c3e7f508e1d6e1d&units=metric';
 
-function weatherData(lat, long)
-{
-    var request = new XMLHttpRequest();
-    request.open('GET','https://openweathermap.org/api',true);
-    request.send();
-    request.onload = function() {
-        var data = JSON.parse(this.response);
-    console.log(lat, long);
-}
-}
-}
+	request.open('GET', url, true);
+	request.send();
+	request.onload = function () {
+		try {
+			var result = JSON.parse(this.response);
+			console.log(`${name} : ${result.main.temp}`);
+		} catch (e) {
+			console.log('undefined response' + name);
+		}
+	};
+};
